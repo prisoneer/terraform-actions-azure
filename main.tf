56f1ae1d04a8aab74eb5a72f1a9dfb0866496368
ivo-ns <prisoneer2@gmail.com>
2023-06-18 03:24:25 +0300

    Adding Persistent Storage

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ terraform {
       version = "=3.59.0"
     }
   }
+  
+  backend "azurerm" {
+	resource_group_name  = "StorageRG"
+	storage_account_name = "taskboardstorageivo"
+	container_name 		 = "taskboardcontainer"
+	key 				 = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
